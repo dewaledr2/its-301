a99d588ad59d8ed54533b27404f1b5e8a281362f
dewaledr2 <104567856+dewaledr2@users.noreply.github.com>
2022-04-28 17:57:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             string[] locations = { "Centennial", "Denver", "Greeley", "Lakewood", "Limerick" };
+
+             var searchResult = Array.Find(locations, element => element.StartsWith("l".ToUpper()));
+
+             Console.WriteLine(searchResult);
+        }
+    }
+}
+
